7cf5d5452ada80c45df829e4924455623d26172a
hanna_nyman <hannanyman.sk@gmail.com>
2018-11-26 21:52:28 +0100

    User visits site test is passing

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -1,7 +1,7 @@
 Given('I visit the site') do
-
+    visit root_path
 end
 
-Then('I should see {string}') do |string|
-
+Then('I should see {string}') do |content|
+    expect(page).to have_content content
 end
