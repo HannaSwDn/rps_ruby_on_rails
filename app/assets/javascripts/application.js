60a06dfed53035d7bd9098554f1db6b884f2edfb
hanna_nyman <hannanyman.sk@gmail.com>
2018-11-28 12:46:16 +0100

    Adding eventlisteners to determine if the user has chosen rock, paper or scissors

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,4 +9,16 @@ document.addEventListener('DOMContentLoaded', event => {
     rock = document.querySelector('#rock')
     paper = document.querySelector('#paper')
     scissors = document.querySelector('#scissors')
+
+    rock.addEventListener('click', event => {
+        userChoice = 'rock'
+    })
+
+    paper.addEventListener('click', event => {
+        userChoice = 'paper'
+    })
+
+    scissors.addEventListener('click', event => {
+        userChoice = 'scissors'
+    })
 })
