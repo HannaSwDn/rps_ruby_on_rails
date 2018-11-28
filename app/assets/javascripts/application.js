2f51d9b2631995413db8b94bbc8a99c14435ea0d
hanna_nyman <hannanyman.sk@gmail.com>
2018-11-28 12:40:51 +0100

    Adding event listener to assign DOM variables when the DOM is loaded

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,1 +1,9 @@
 // DOM variables
+let rock, paper, scissors
+
+// setting DOM variables when the DOM is loaded
+document.addEventListener('DOMContentLoaded', event => {
+    rock = document.querySelector('#rock')
+    paper = document.querySelector('#paper')
+    scissors = document.querySelector('#scissors')
+})
