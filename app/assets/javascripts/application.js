1a31400ecc36b52b4843738cfe2d93b0e0f44eb9
hanna_nyman <hannanyman.sk@gmail.com>
2018-11-28 12:50:39 +0100

    Creating generateComputerChoice function, calling the function every time the user chooses rock, paper or scissors

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,13 +12,21 @@ document.addEventListener('DOMContentLoaded', event => {
 
     rock.addEventListener('click', event => {
         userChoice = 'rock'
+        generateComputerChoice()
     })
 
     paper.addEventListener('click', event => {
         userChoice = 'paper'
+        generateComputerChoice()
     })
 
     scissors.addEventListener('click', event => {
         userChoice = 'scissors'
+        generateComputerChoice()
     })
 })
+
+const generateComputerChoice = () => {
+    let number = Math.floor(Math.random() * 3 + 1)
+    alert(number)
+}
