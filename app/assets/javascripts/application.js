6d2981e86022358745817adbe9e27016ed3e57c8
hanna_nyman <hannanyman.sk@gmail.com>
2018-11-28 12:45:50 +0100

    Adding points variables for computer and user, adding variable to set the user's choice and the computer's choice

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,9 @@
 // DOM variables
 let rock, paper, scissors
 
+// global variables
+let userPoints, computerPoints, userChoice, computerChoice
+
 // setting DOM variables when the DOM is loaded
 document.addEventListener('DOMContentLoaded', event => {
     rock = document.querySelector('#rock')
