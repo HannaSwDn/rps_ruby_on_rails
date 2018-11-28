3e674de67a69c97040f541525b717b621370d5f9
hanna_nyman <hannanyman.sk@gmail.com>
2018-11-28 12:53:41 +0100

    Adding switch statement to re-assign computerChoice integer to string

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,6 +27,17 @@ document.addEventListener('DOMContentLoaded', event => {
 })
 
 const generateComputerChoice = () => {
-    let number = Math.floor(Math.random() * 3 + 1)
-    alert(number)
+    computerChoice = Math.floor(Math.random() * 3 + 1)
+    
+    switch(computerChoice) {
+        case 1:
+            computerChoice = 'rock'
+            break
+        case 2:
+            computerChoice = 'paper'
+            break
+        case 3:
+            computerChoice = 'scissors'
+            break
+    }
 }
